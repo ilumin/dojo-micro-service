4f75d57c99a70c816813fca26051dac1674cb9e8
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-10-24 21:18:48 +0700

    ADD home controller

--- src/main/java/com/ilumin/dojo/MicroServiceApplication.java
+++ src/main/java/com/ilumin/dojo/MicroServiceApplication.java
@@ -2,6 +2,8 @@ package com.ilumin.dojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MicroServiceApplication {
@@ -9,4 +11,12 @@ public class MicroServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroServiceApplication.class, args);
 	}
+}
+
+@RestController
+class HomeController {
+    @GetMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 }
